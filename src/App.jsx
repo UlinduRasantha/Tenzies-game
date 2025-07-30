a73509cd437caa503a1b8f25b7ef9f3ed427fe3b
URSamarappuli <22ug1-0465@sltc.ac.lk>
2025-07-30 20:23:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
-export default function() {
-    return(
+import { useState } from "react"
+import Die from "./component/Die"
+
+export default function App() {
+    
+    const [dice, setDice] = useState(generateAllNewDice())
+    
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+    
+    const diceElements = dice.map(num => <Die value={num} />)
+    
+    return (
         <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
         </main>
     )
 }
